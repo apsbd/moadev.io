7f2f606bd6beebf2e424fea90ffcbc5a9e5ffef5
Mohiuddin Ahmed <mohiuddin@tieredcommunication.com>
2026-02-17 00:24:00 +0600

    wip: home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,52 @@
 import { Button } from '@/components/ui/button';
+import { FiDownload } from 'react-icons/fi';
 
-export default function Home() {
-    return <main>home page</main>;
-}
+// Components
+import Socials from '@/components/Socials';
+
+const Home = () => {
+    return (
+        <section className='h-full'>
+            <div className='container mx-auto h-full px-4'>
+                <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+                    {/* text */}
+                    <div className='text-center xl:text-left'>
+                        <span className='text-xl'>Software Developer</span>
+                        <h1 className='h1 mb-6'>
+                            Hello I'm <br />{' '}
+                            <span className='text-accent'>Mohiuddin Ahmed</span>
+                        </h1>
+                        <p className='max-w-125 mb-9 text-white/80'>
+                            I am a software developer with experience in
+                            building web applications using modern technologies.
+                            I am passionate about learning new technologies and
+                            improving my skills. I am always looking for new
+                            opportunities to grow and develop as a software
+                            developer.
+                        </p>
+                        {/* buttons & socials */}
+                        <div className='flex flex-col xl:flex-row items-center gap-8'>
+                            <Button
+                                variant='outline'
+                                size='lg'
+                                className='uppercase flex items-center gap-2'>
+                                <span>Download CV</span>
+                                <FiDownload className='text-xl' />
+                            </Button>
+                            <div className='mb-8 xl:mb-0'>
+                                <Socials
+                                    containerStyles='flex gap-6'
+                                    iconStyles='w-9 h-9 border border-accent rounded-full flex items-center justify-center text-accent text-base hover:bg-accent hover:text-background hover:transition-all duration-500'
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    {/* photo */}
+                    <div>photo</div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -10,16 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-accent text-background hover:bg-accent-hover",
+        primary: "bg-primary text-white",
+        outline: "border border-accent bg-transparent text-accent hover:bg-accent hover:text-background",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-        "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-8",
-        "icon-lg": "size-10",
+        default: "h-[44px] px-6",
+        md: "h-[48px] px-6",
+        lg: "h-[56px] px-8 text-sm uppercase tracking-[2px]",
       },
     },
     defaultVariants: {
